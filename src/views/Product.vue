334c0d65d5649c5041763c5030dd3078b452ab3c
KevinWu <a96321452@gms.tku.edu.tw>
2020-04-09 00:23:04 +0800

    Add Vuetify and eslintrc

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="product">
     <div class="tissue">
-      <img class="tissue_head" src="../assets/img/product/tissue_head.png" alt="">
+      <img
+        class="tissue_head"
+        src="../assets/img/product/tissue_head.png"
+        alt=""
+      />
       <div class="product_field">
-          <img src="../assets/img/product/300ml.jpg" alt="">
-          <img src="../assets/img/product/600ml.jpg" alt="">
-          <img src="../assets/img/product/1000ml.jpg" alt="">
-          <img src="../assets/img/product/600ml.jpg" alt="">
-          <img src="../assets/img/product/600ml.jpg" alt="">
+        <img src="../assets/img/product/300ml.jpg" alt="" />
+        <img src="../assets/img/product/600ml.jpg" alt="" />
+        <img src="../assets/img/product/1000ml.jpg" alt="" />
+        <img src="../assets/img/product/600ml.jpg" alt="" />
+        <img src="../assets/img/product/600ml.jpg" alt="" />
       </div>
     </div>
   </div>
@@ -18,5 +22,4 @@ export default {
 }
 </script>
 
-<style lang="scss" src="@/assets/css/product.scss" scoped>
-</style>
+<style lang="scss" src="@/assets/css/product.scss" scoped></style>
