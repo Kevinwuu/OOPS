885a964f480502b57a1014ed26935ad364e293db
KevinWu <a96321452@gms.tku.edu.tw>
2020-04-11 16:55:53 +0800

    Add page transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <Header/>
-    <router-view/>
-    <Footer/>
+    <Header />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+    <Footer />
   </div>
 </template>
 <script>
@@ -17,3 +19,16 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
