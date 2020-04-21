6204d84958fbf85a38c7d1d40a1203d737a7927b
KevinWu <a96321452@gms.tku.edu.tw>
2020-04-22 01:29:21 +0800

    home page animate test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,13 @@
       <div class="oops">
         <img src="../assets/img/home/oops.png" alt="">
       </div>
-      <div class="aboutus" data-aos="fade-left">
+      <div class="aboutus" data-aos="fade-right">
           <img src="../assets/img/home/text_aboutus.png" alt="" />
+          <img id="about" src="../assets/img/home/img_about.jpg" data-aos="fade-left" data-aos-anchor=".product" alt="">
       </div>
       <div class="product" data-aos="fade-up">
           <img data-aos="fade-up" src="../assets/img/home/text_product.png" alt="" />
-          <!-- <img class="product_desc" src="../assets/img/home/img_product_desc.jpg" alt=""> -->
+          <!-- <img id="product" src="../assets/img/home/img_product_desc.jpg" data-aos="fade-left" data-aos-anchor=".product" alt=""> -->
       </div>
       <div class="guideline">
           <img src="../assets/img/home/text_guideline.png" alt="" />
