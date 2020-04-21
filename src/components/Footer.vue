56392bf51fa4517459a96de9cf33818155a6e89d
KevinWu <a96321452@gms.tku.edu.tw>
2020-04-21 21:03:08 +0800

    Add Footer hover style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,45 @@
 <template>
-    <footer>
-        <div class="social_media">
-            <div>
-                <img src="../assets/img/footer/text_social_websites.png" alt="" />
-            </div>
-            <div class="social_icon">
-                <a v-for="(item) in social" :href="item.link" :key="item.id" target="_blank">
-                    <img :src="item.path" alt="">
-                </a>
-            </div>
-        </div>
-        <div class="nav_link">
-            <ul>
-                <li v-for="(path) in menu" :key="path.id">
-                    <img :src="path" alt="" />
-                </li>
-            </ul>
-        </div>
-        <div class="contact">
-            <ul>
-                <li v-for="item in contact" :key="item.id">
-                    <img :src="item.title" alt="" />
-                    <p>
-                        <img :src="item.info" alt="" />
-                    </p>
-                </li>
-            </ul>
-        </div>
-        <div class="logo">
-            <router-link to="/">
-                <img src="../assets/img/logo.png" alt="" />
-            </router-link>
-        </div>
-    </footer>
+  <footer>
+    <div class="social_media">
+      <div>
+        <img src="../assets/img/footer/text_social_websites.png" alt="" />
+      </div>
+      <div class="social_icon">
+        <a
+          v-for="item in social"
+          :href="item.link"
+          :key="item.id"
+          target="_blank"
+        >
+          <img :src="item.path" alt="" />
+        </a>
+      </div>
+    </div>
+    <div class="nav_link">
+      <ul>
+        <li v-for="item in menu" :key="item.id">
+          <router-link :to="item.path">
+            <img :src="item.src" alt="" />
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="contact">
+      <ul>
+        <li v-for="item in contact" :key="item.id">
+          <img :src="item.title" alt="" />
+          <p>
+            <img :src="item.info" alt="" />
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="logo">
+      <router-link to="/">
+        <img src="../assets/img/logo.png" alt="" />
+      </router-link>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -55,17 +62,32 @@ import EmailInfo from '@/assets/img/footer/text_email_info.png'
 
 export default {
   name: 'Footer',
-  data () {
+  data() {
     return {
       message: 'xdd',
+    //   menu: [Index, AboutUs, Guideline, Product],
       menu: [
-        Index, AboutUs, Guideline, Product
-      ],
+          {path:'/', src:Index},
+          {path:'/aboutus',src:AboutUs},
+          {path:'/guideline',src:Guideline},
+          {path:'/product',src:Product}
+    ],
       social: [
         { link: 'https://lin.ee/gk9O1wC', path: Line },
-        { link: 'https://m.youtube.com/channel/UCQR-Wq0hiRPERUcKjs-y_Eg/featured', path: Youtube },
-        { link: 'https://m.facebook.com/Oops2019-110233197082200/?modal=admin_todo_tour', path: Facebook },
-        { link: 'https://www.instagram.com/_oops2019/?hl=zh-tw', path: Instagram }
+        {
+          link:
+            'https://m.youtube.com/channel/UCQR-Wq0hiRPERUcKjs-y_Eg/featured',
+          path: Youtube
+        },
+        {
+          link:
+            'https://m.facebook.com/Oops2019-110233197082200/?modal=admin_todo_tour',
+          path: Facebook
+        },
+        {
+          link: 'https://www.instagram.com/_oops2019/?hl=zh-tw',
+          path: Instagram
+        }
       ],
       contact: [
         { title: Address, info: AddressInfo },
@@ -77,5 +99,4 @@ export default {
 }
 </script>
 
-<style scoped src="@/assets/css/footer.scss" lang="scss">
-</style>
+<style scoped src="@/assets/css/footer.scss" lang="scss"></style>
